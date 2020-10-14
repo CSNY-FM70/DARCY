d3e42a9a8e2fa35e7c14cf37f8d50e5d5ec4a850
FlyONtheWall742 <gheredia.crypto@gmail.com>
2020-10-14 11:52:28 +0200

    Eigen::ConjugateGradient

--- libdarcy/src/FEM_solver.cpp
+++ libdarcy/src/FEM_solver.cpp
@@ -1,26 +1,24 @@
-namespace FEM
-{
+namespace FEM{
 
 template <typename T, int K>
-Solver<T,K>::Solver(typename Linear::Solver<T,(K-1)*(K+1)>& lsolver):_lsolver(lsolver){}
+Solver<T,K>::Solver(){}
 
 template<typename T,int K>
 void Solver<T,K>::solve(System<T, K>& femsys) {
- 	const int n_int = femsys.revised_int_nodes.size();
- 	const int n_bound = femsys.Dirichlet_nodes.size();
+ 	const int n_int = (K+1)*(K-1);
+ 	const int n_bound = (K+1)*2;
  	const int nvtx = (K+1)*(K+1);
- 	Linear::System<T,(K-1)*(K+1)> lsys(n_int);
- 	lsys.A() = femsys.get_A();
- 	lsys.b() = femsys.get_b();
- 	_lsolver.solve(lsys);
+ 	CG.compute(femsys.get_A());
+	Eigen::Matrix<T,Eigen::Dynamic,1> u_int = Eigen::Matrix<T,n_int,1>::Zero();
+	u_int = CG.solve(femsys.get_b());
  	femsys.get_p().resize(nvtx,1);
  	//Adding solution for interior nodes
 	for(int i=0;i<n_int;i++){
-	  	femsys.get_p()(femsys.revised_int_nodes(i)) = lsys.x()(i);
+	  	femsys.get_p()(femsys.interior.at(i)) = u_int(i);
  	}
 	//Correcting solution on Dirichlet boundaries
  	for(int j=0;j<n_bound;j++){
-	 	 femsys.get_p()(femsys.Dirichlet_nodes(j)) = femsys.DwB(j);
+	 	 femsys.get_p()(femsys.boundary.at(j)) = femsys.bound_data.at(j);
   	}
 }
 
